0e1d1ce1f5e24f5bc991ef924c54844d8055b977
Anmol Singh <anmolthedeveloper@gmail.com>
2019-10-19 20:40:21 +0530

    added bootstrap

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
   <head>
     <meta charset="utf-8">
     <title>FriendsMaker</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   </head>
   <body>
-
+    <p class="alert alert-success">hello world</p>
   </body>
 </html>
