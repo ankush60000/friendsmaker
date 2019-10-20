d614064b7f28a3e27df226dfb7690feba223e348
Anmol Singh <anmolthedeveloper@gmail.com>
2019-10-20 19:36:34 +0530

    created menu and added a temp logo

---
+++ add-ons/main-menu.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><img src="img/logo.png" height="20%" width="20%"></a>
+    <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarResponsive" type="button">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarResponsive">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Help</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -8,13 +8,12 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
   </head>
   <body>
-    <?php include "add-ons/" ?>
+    <!-- Navigation -->
+    <?php include "add-ons/main-menu.php"; ?>
     <div class="container">
     </div>
     <script src="bootstrap/jquery-3.4.1.min.js"></script>
-    <script src="bootstrap/opper.min.js">
-
-    </script>
+    <script src="bootstrap/opper.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
   </body>
 </html>
