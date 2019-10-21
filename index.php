d9e60a6991785e44ac1a1804220420d909283aa8
Anmol Singh <anmolthedeveloper@gmail.com>
2019-10-21 20:49:33 +0530

    check

--- index.php
+++ index.php
@@ -19,10 +19,7 @@
     <?php include "add-ons/main-menu.php"; ?>
     <div class="container">
     </div>
-<<<<<<< HEAD
-=======
 
->>>>>>> 0932616606e46ba7051c7ac140241b7d05fe6ce8
     <?php include "add-ons/footer.php"; ?>
     <script src="bootstrap/jquery-3.4.1.min.js"></script>
     <script src="bootstrap/popper.min.js"></script>
