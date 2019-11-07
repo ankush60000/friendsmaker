0fd56d73afd4ccc23d0b103cac11e8b4a52a1020
Anmol Singh <anmolthedeveloper@gmail.com>
2019-11-07 11:50:14 +0530

    carousel created

--- index.php
+++ index.php
@@ -5,22 +5,40 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Friends Maker</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <style media="screen">
-      body{
-        height: 2000px;
-        position: relative;
-      }
-    </style>
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="style.css">
   </head>
-  <body>
-    <!-- Navigation -->
-    <?php include "add-ons/main-menu.php"; ?>
-    <div class="container">
-    </div>
-    <?php include "add-ons/footer.php"; ?>
-    <script src="bootstrap/jquery-3.4.1.min.js"></script>
-    <script src="bootstrap/popper.min.js"></script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-  </body>
-</html>
+    <body>
+      <!-- Navigation -->
+      <?php include "add-ons/main-menu.php"; ?>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel" data-pause="false">
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/social.jpg" class="d-block w-100 img-fluid" alt="...">
+          </div>
+          <div class="carousel-item cus-img">
+            <img src="img/social3.jpg" class="d-block w-100 img-fluid" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/social2.jpg" class="d-block w-100 img-fluid" alt="...">
+          </div>
+        </div>
+      </div>
+      <div class="container">
+
+      </div>
+      <?php include "add-ons/footer.php"; ?>
+      <script src="bootstrap/jquery-3.4.1.min.js"></script>
+      <script src="bootstrap/popper.min.js"></script>
+      <script src="bootstrap/js/bootstrap.min.js"></script>
+      <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> -->
+    </body>
+  </html>
